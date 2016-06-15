6e66ce15dc940582838fc573284ee6778fc482da
Libern <libernlin@gmail.com>
2016-06-15 21:56:12 +0800

    Improved tests

--- tests/RestClientTest.php
+++ tests/RestClientTest.php
@@ -7,10 +7,17 @@ class RestClientTest extends TestCase
     public function testRestClient()
     {
         $restClient = new \Libern\Rest\RestClient();
+        $restClient->setOAuthUserCredentials([
+            'username' => 'libern@someline.com',
+            'password' => 'Abc12345',
+        ]);
         $restClient->withOAuthTokenTypeUser();
         $response = $restClient->get("users");
-        $restClient->printResponseData();
-        $restClient->getResponseData();
+        if ($response->getStatusCode() == 200) {
+            $responseData = $restClient->getResponseData();
+        } else {
+            $restClient->printResponseOriginContent();
+        }
         $this->assertEquals($response->getStatusCode(), 200);
     }
 
